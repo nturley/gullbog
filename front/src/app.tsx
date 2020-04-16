f567c42e6ea5e281396bce972398d9c933c60784
Joe Williams <josephscottwilliams@gmail.com>
2020-04-16 18:23:49 -0500

    initial setup and hello world test

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -3,7 +3,8 @@ import * as ReactDOM from "react-dom";
 
 class App extends React.Component {
     render() {
-        return <h1>Hello World!</h1>;
+        return <h1>Hello World, what's up?! 
+        </h1>;
     }
 }
 
