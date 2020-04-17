0fc91a9d907b886b9f22deaefe5cc14d3766b01a
Joe Williams <josephscottwilliams@gmail.com>
2020-04-17 17:19:02 -0500

    minor tests and changes to header, added style.css

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import "./style.css";
+
 
 class App extends React.Component {
     render() {
-        return <h1>Hello World, what's up?! 
-        </h1>;
+        return <div>
+            <div className="header" >
+                <h1 style={{color: "red"}}>Gullbog</h1>
+
+            </div>
+        </div>;
     }
 }
 
